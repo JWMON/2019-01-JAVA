0f25f064e6dc0ae98edb03976dfde2907def1542
JWMON <48053063+JWMON@users.noreply.github.com>
2019-04-25 16:23:58 +0900

    Lab12

---
+++ Lab12/edu/handong/csee/java/string/FamilyNamePrinter.java
@@ -0,0 +1,33 @@
+package edu.handong.csee.java.string;
+
+class FamilyNamePrinter{
+
+    private String[] stringNames;
+    private String[] familyNames;
+
+    FamilyNamePrinter(String sng){
+
+    }
+    public static void main(String args[]){
+        String stringNames = args;
+        String[] names = stringNames.split(", ");
+        String[] familyNames = new String[names.length];
+        System.out.println(names.length);
+
+        for(int i=0; i<names.length; i++){
+            familyNames[i] = names[i].split(" ")[1];
+        }
+
+        for(String familyName : familyNames){
+            System.out.println(familyName);
+        }
+    }
+
+    // public void printFamilyNames(){
+        
+    // }
+    
+
+
+
+}
